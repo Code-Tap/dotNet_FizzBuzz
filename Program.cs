87edc3966165777d5461971ca9014a1d95250390
Leigh Wilson <code-tap@leighwilson.xyz>
2021-11-03 12:29:05 +0000

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Fizzbuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            string Buzzer(int i) {
+                string msg = "";
+                if ( i % 3 == 0) { msg += "Fizz"; }
+                if ( i % 5 == 0) { msg += "Buzz"; }
+                return (msg);
+            }
+
+            for ( int i = 0; i < 20; i++)
+                Console.WriteLine($"{i} :: {Buzzer(i)}");
+        }
+    }
+}
